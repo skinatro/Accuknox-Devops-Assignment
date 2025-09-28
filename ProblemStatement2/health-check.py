a49ab3d1f4e888bdad20964e4ce7293e57a34a04
Skinatro <work.omkate@gmail.com>
2025-09-28 16:52:45 +0530

    PS1 complete

--- ProblemStatement2/health-check.py
+++ ProblemStatement2/health-check.py
@@ -3,7 +3,7 @@
 import requests
 
 try:
-    r = requests.head("https://www.google.com", timeout=5)
+    r = requests.head("http://k8s.skinatro.tech", timeout=5)
     if 200 <= r.status_code < 400:
         print(f"Up! Status: {r.status_code}")
     else:
